12d9f6b82ed71e65f389aae47f399468d9b826c7
S. HSUEH <41102113+juniorlove@users.noreply.github.com>
2018-08-14 20:03:10 +0800

    Add files via upload

---
+++ 0711_scriptl.py
@@ -0,0 +1,7 @@
+# A frist Python script
+import sys					# load a lobrary module
+print(sys.platform)
+print(2 ** 100)				# raise 2 to a power
+x = 'Spsm!'
+print(x * 8)				# string repetition
+input()
